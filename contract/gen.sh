c92f2d4afb105ac2899c44144d6906d9329b1028
qct <tango.qct@gmail.com>
2023-03-12 15:56:29 +0400

    Generate AssetGovernanceGen

--- contract/gen.sh
+++ contract/gen.sh
@@ -2,5 +2,6 @@
 
 abigen --abi=ZkBNB.json --pkg=core --type ZkBNB --out=../core/ZkBNBGen.go
 abigen --abi=Governance.json --pkg=core --type Governance --out=../core/GovernanceGen.go
+abigen --abi=AssetGovernance.json --pkg=core --type AssetGovernance --out=../core/AssetGovernanceGen.go
 abigen --abi=./ERC20.json --bin ./ERC20.bin --pkg=core --type ERC20 --out=../core/ERC20Gen.go
 abigen --abi=./ERC721A.json --bin ./ERC721A.bin --pkg=core --type ERC721 --out=../core/ERC721Gen.go
