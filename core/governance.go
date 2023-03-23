88765330f37391fc9ddfb0f9dce777691963e214
markc <mark.c@binance.com>
2023-03-23 14:04:16 +0300

    add ActivateDesertMode

--- core/governance.go
+++ core/governance.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/bnb-chain/zkbnb-eth-rpc/rpc"
@@ -9,4 +10,13 @@ import (
 func LoadGovernanceInstance(cli *rpc.ProviderClient, addr string) (instance *Governance, err error) {
 	instance, err = NewGovernance(common.HexToAddress(addr), *cli)
 	return instance, err
+}
+
+func ValidateAssetAddress(instance *Governance, assetAddr common.Address,
+) (uint16, error) {
+	uint16, err := instance.ValidateAssetAddress(&bind.CallOpts{}, assetAddr)
+	if err != nil {
+		return 0, err
+	}
+	return uint16, nil
 }
