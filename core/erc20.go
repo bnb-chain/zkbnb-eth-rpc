22b59b232dfe459f6a58248fc9f096a957c52191
markc <mark.c@binance.com>
2023-03-29 23:43:39 +0300

    add BalanceOf

--- core/erc20.go
+++ core/erc20.go
@@ -1,7 +1,9 @@
 package core
 
 import (
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"math/big"
 
 	"github.com/bnb-chain/zkbnb-eth-rpc/rpc"
 )
@@ -9,4 +11,12 @@ import (
 func LoadERC20(cli *rpc.ProviderClient, addr string) (instance *ERC20, err error) {
 	instance, err = NewERC20(common.HexToAddress(addr), *cli)
 	return instance, err
+}
+
+func BalanceOf(instance *ERC20, address common.Address, assetAddr common.Address) (*big.Int, error) {
+	amount, err := instance.BalanceOf(&bind.CallOpts{From: address}, assetAddr)
+	if err != nil {
+		return nil, err
+	}
+	return amount, nil
 }
